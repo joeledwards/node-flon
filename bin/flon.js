6a6feffd267a300cb584413ed2ca2bd0ee0c62cd
Joel Edwards <joeledwards@gmail.com>
2018-01-26 23:10:38 -0700

    feat: very simple, incomplete JSON flattening is now supported

--- bin/flon.js
+++ bin/flon.js
@@ -1,7 +1,57 @@
+#!/usr/bin/env node
+
 const yargs = require('yargs')
 
 function config () {
-  const yargs
+  return yargs
     .env('FLON')
+    .option('file', {
+      type: 'string',
+      desc: 'read from specified file instead of stdin',
+      alias: ['f']
+    })
+    .option('url', {
+      type: 'string',
+      desc: 'read from specified URL instead of stdin',
+      alias: ['u'],
+      conflicts: ['file']
+    })
+    .help()
     .argv
 }
+
+async function run () {
+  const r = require('ramda')
+  const unmarshal = require('../lib/unmarshal')
+
+  let source = 'stdin'
+  try {
+    const {file, url} = config()
+    const output = process.stdout
+
+    if (file) {
+      source = file
+      const fs = require('fs')
+      await unmarshal(fs.createReadStream(file), output)
+    } else if (url) {
+      source = url
+      const axios = require('axios')
+      const {data} = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+      })
+      await unmarshal(data, output)
+    } else {
+      process.stdin.resume()
+      await unmarshal(process.stdin, output)
+    }
+  } catch (error) {
+    console.error(
+      `Error reading content from ${source ? source : 'stdin'} :`,
+      error
+    )
+  }
+}
+
+run()
